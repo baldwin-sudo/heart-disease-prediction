bb805f81d0c568a33bd2652f49b65df0616d781a
baldwin-sudo <kasmimehdi561@gmail.com>
2024-10-31 22:20:56 +0100

    modified abstract

--- frontend/src/Abstract.jsx
+++ frontend/src/Abstract.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 import "./Abstract.css";
 function Abstract() {
-  return <div>Abstract</div>;
+  return (
+    <div className="abstract">
+      <h2>Abstract</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti beatae
+        veniam veritatis odit, nobis nemo dicta quidem tenetur, debitis, dolorum
+        necessitatibus maxime reiciendis omnis atque! Quaerat dolor enim error
+        alias? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Doloremque, excepturi dolorum. Nobis rem eligendi recusandae tempore
+        alias dolor, dignissimos ipsam consequuntur esse porro ipsa dolore,
+        ratione cupiditate delectus amet. Fugit?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore
+        dolores et, aliquam iusto facilis earum nemo consequatur aliquid
+        doloribus assumenda, omnis sequi commodi architecto distinctio dolorem,
+        voluptatibus obcaecati. Sed, atque? Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Ex minus necessitatibus sunt ea nemo magni
+        dicta quasi recusandae officia debitis tenetur molestiae, reprehenderit
+        magnam architecto minima id neque, dolorum repudiandae.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque enim,
+        libero natus sequi voluptas distinctio maiores cupiditate quia
+        consequatur temporibus, vitae obcaecati veniam nostrum! Dignissimos
+        veniam in aspernatur voluptatum similique! Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Commodi id enim accusantium perspiciatis
+        officiis explicabo quisquam, aperiam qui soluta error alias fugiat eum
+        dolore aspernatur nostrum tenetur voluptates minima blanditiis?
+      </p>
+    </div>
+  );
 }
 
 export default Abstract;
